fab7b4a7445818d100c063e15e15b7b14412fbab
Todd Wolfson <todd@twolfson.com>
2013-08-13 02:44:24 -0700

    Fixing variable quoting

--- test/before_script.sh
+++ test/before_script.sh
@@ -1,5 +1,5 @@
 # If were are not on the bleeding edge git
-if $GIT_PPA != "ppa:pdoes/ppa"; then
+if "$GIT_PPA" != "ppa:pdoes/ppa"; then
   # then use the Ubuntu one
   sudo apt-get install ppa-purge -y
   sudo ppa-purge "ppa:pdoes/ppa" -y
