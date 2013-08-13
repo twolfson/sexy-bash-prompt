2af8a02f02e6b71ca16c35fb22330bf79675a55b
Todd Wolfson <todd@twolfson.com>
2013-08-13 02:43:04 -0700

    Attempting to use ppa-purge for downgrading

--- test/before_script.sh
+++ test/before_script.sh
@@ -1,2 +1,6 @@
-echo $GIT_PPA
-git --version
+# If were are not on the bleeding edge git
+if $GIT_PPA != "ppa:pdoes/ppa"; then
+  # then use the Ubuntu one
+  sudo apt-get install ppa-purge -y
+  sudo ppa-purge "ppa:pdoes/ppa" -y
+fi
