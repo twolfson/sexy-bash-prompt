f7a96ba6df7b953b3165a25c40b950256271f343
Todd Wolfson <todd@twolfson.com>
2013-08-13 23:11:41 -0700

    Reading in fixture fine

--- test/install_test.sh
+++ test/install_test.sh
@@ -1,8 +1,15 @@
 # Install the prompt
 # make install
 
-# Get the output of a new shell
-INSTALLED_PS1="$(bash -i -c 'echo $PS1')"
+# Set up test constants
+EXPECTED_FILE="test/test-files/ps1/expected.txt"
 
-# Save a fixture
-echo "$INSTALLED_PS1" > test/test-files/ps1/expected.txt
+# Get the output of a new shell
+ACTUAL_PS1="$(bash -i -c 'echo $PS1')"
+
+# # Save a fixture
+# echo "$ACTUAL_PS1" > $EXPECTED_FILE
+
+# Assert the ACTUAL_PS1 matches the expected PS1
+EXPECTED_PS1="$(cat $EXPECTED_FILE)"
+echo "$EXPECTED_PS1"
