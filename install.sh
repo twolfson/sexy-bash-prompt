d986fdd66be0033df0a64461ef813f480cc99442
Todd Wolfson <todd@twolfson.com>
2013-08-14 00:34:04 -0700

    Beginning to fill out install skeleton

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 # Get the current PS1
+get_ps1 () {
+  bash -i -c 'echo $PS1'
+}
 
 # If already contains our current prompt, leave
+if [[ -n "$(get_ps1 | grep get_git_info)" ]]; then
+  exit 0
+fi
+
+echo hi
 
 # If .bashrc does not exist OR does not contain .bash_prompt invocation
   # Add the .bash_prompt invocation to .bashrc
