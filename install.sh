ec6e910e980a025e65e1b62865e722a841c3d951
Todd Wolfson <todd@twolfson.com>
2013-08-14 01:24:31 -0700

    Moving to short/long formats for PROFILE_SCRIPT

--- install.sh
+++ install.sh
@@ -21,22 +21,25 @@ ps1_is_installed () {
 # ps1_is_installed && exit 0
 
 # By default, .bash_profile is our profile script
-PROFILE_SCRIPT="~/.bash_profile"
+PROFILE_SCRIPT_SHORT="~/.bash_profile"
+PROFILE_SCRIPT_FULL=~/.bash_profile
 
 # Find which exists .bash_profile, .bash_login, or .profile
 if [[ -f ~/.bash_profile ]]; then
-  PROFILE_SCRIPT="~/.bash_profile"
+  # Use defaults
 elif [[ -f ~/.bash_login ]]; then
-  PROFILE_SCRIPT="~/.bash_login"
+  PROFILE_SCRIPT_SHORT="~/.bash_login"
+  PROFILE_SCRIPT_FULL=~/.bash_login
 elif [[ -f ~/.profile ]]; then
-  PROFILE_SCRIPT="~/.profile"
+  PROFILE_SCRIPT_SHORT="~/.profile"
+  PROFILE_SCRIPT_FULL=~/.profile
 fi
 
 # Add a bash invocation to the profile script
-echo "# Adding ~/.bashrc triggers to $PROFILE_SCRIPT"
-echo "# Trigger ~/.bashrc commands" >> $PROFILE_SCRIPT
-echo ". ~/.bashrc" >> $PROFILE_SCRIPT
+echo "# Adding ~/.bashrc triggers to $PROFILE_SCRIPT_SHORT"
+echo "# Trigger ~/.bashrc commands" >> "$PROFILE_SCRIPT_FULL"
+echo ". ~/.bashrc" >> "$PROFILE_SCRIPT_FULL"
 
 # If our prompt is not being loaded, notify the user and leave angrily
 ps1_is_installed || (echo "sexy-bash-prompt was added to ~/.bashrc \
-and $PROFILE_SCRIPT but is not being picked up by bash." 1>&2 && exit 1)
+and $PROFILE_SCRIPT_SHORT but is not being picked up by bash." 1>&2 && exit 1)
