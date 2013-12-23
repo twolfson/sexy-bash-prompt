8c7603f56b471d16d02358da2a6c72e88e3ce66b
Todd Wolfson <todd@twolfson.com>
2013-12-22 16:50:00 -0800

    Attempting to debug bad PS1 match

--- test/prompt_test.sh
+++ test/prompt_test.sh
@@ -186,7 +186,7 @@ esc=$'\033'
   TERM=xterm-256color PROMPT_USER_COLOR='\033[1;32m' PROMPT_PREPOSITION_COLOR='\033[1;33m' PROMPT_DEVICE_COLOR='\033[1;34m' PROMPT_DIR_COLOR='\033[1;35m' PROMPT_GIT_STATUS_COLOR='\033[1;36m' PROMPT_SYMBOL_COLOR='\033[1;37m' . .bash_prompt
 
     # use the new colors
-    expected_prompt='\[\033[1;32m\]\u\[\033[m\] \[\033[1;33m\]at\[\033[m\] \[\033[1;34m\]\h\[\033[m\] \[\033[1;33m\]in\[\033[m\] \[\033[1;35m\]\w\[\033[m\]$( is_on_git &&   echo -n " \[\033[1;36m\]on\[\033[m\] " &&   echo -n "\[\033[1;37m\]$(get_git_info)" &&   echo -n "\[\033[1;33m\]")\n\[\033[m\]\[\]$ \[\033[m\]'
+    expected_prompt='\[\033[1;32m\]\u\['$esc'(B'$esc'[m\] \[\033[1;33m\]at\['$esc'(B'$esc'[m\] \[\033[1;34m\]\h\['$esc'(B'$esc'[m\] \[\033[1;33m\]in\['$esc'(B'$esc'[m\] \[\033[1;35m\]\w\['$esc'(B'$esc'[m\]$( is_on_git &&   echo -n " \[\033[1;36m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\[\033[1;37m\]$(get_git_info)" &&   echo -n "\[\033[1;33m\]")\n\['$esc'(B'$esc'[m\]\[\]$ \['$esc'(B'$esc'[m\]'
     echo $PS1
     echo $expected_prompt
     test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (overridden)' 1>&2
