aed2728854a87c5de5758b0085acf4a755faf6bb
Todd Wolfson <todd@twolfson.com>
2014-01-16 04:24:10 -0800

    Moved script addition to profile script into a conditional block

--- install.sh
+++ install.sh
@@ -32,13 +32,22 @@ elif [[ -f ~/.profile ]]; then
   PROFILE_SCRIPT_FULL=~/.profile
 fi
 
-# If the current script already has notes about .bashrc, be cautious and tell the user
+# If the current script does not have notes about .bashrc
+if ! grep "$PROFILE_SCRIPT_FULL" .bashrc &> /dev/null;
+  # Add a bash invocation to the profile script
+  echo "# Adding ~/.bashrc triggers to $PROFILE_SCRIPT_SHORT"
+  echo "# Trigger ~/.bashrc commands" >> "$PROFILE_SCRIPT_FULL"
+  echo ". ~/.bashrc" >> "$PROFILE_SCRIPT_FULL"
 
-# Add a bash invocation to the profile script
-echo "# Adding ~/.bashrc triggers to $PROFILE_SCRIPT_SHORT"
-echo "# Trigger ~/.bashrc commands" >> "$PROFILE_SCRIPT_FULL"
-echo ". ~/.bashrc" >> "$PROFILE_SCRIPT_FULL"
-
-# If our prompt is not being loaded, notify the user and leave angrily
-ps1_is_installed || (echo "sexy-bash-prompt was added to ~/.bashrc \
-and $PROFILE_SCRIPT_SHORT but is not being picked up by bash." 1>&2 && exit 1)
+  # If our prompt is not being loaded, notify the user and leave angrily
+  ps1_is_installed || (echo "sexy-bash-prompt was added to ~/.bashrc \
+  and $PROFILE_SCRIPT_SHORT but is not being picked up by bash." 1>&2 && exit 1)
+# Otherwise, notify the user about how to add it but do nothing
+else
+  echo "# We cannot confirm that `sexy-bash-prompt` has installed properly" 1>&2
+  echo "# Please open a new tab to confirm" 1>&2
+  echo "" 1>&2
+  echo "# If it has not, please run the following code:" 1>&2
+  echo "echo \". ~/.bashrc\" >> \"$PROFILE_SCRIPT_FULL\"" 1>&2
+  exit 1
+fi
