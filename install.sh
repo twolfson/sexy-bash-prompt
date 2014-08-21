cdc760eecc69883329fc7c2ff6e08b33cfeaf49a
Ryan Delaney <ryan.delaney@gmail.com>
2014-08-21 15:36:31 -0700

    Using lower-case names for locally scoped variables

--- install.sh
+++ install.sh
@@ -19,37 +19,37 @@ echo ". ~/.bash_prompt" >> ~/.bashrc
 ps1_is_installed && exit 0
 
 # By default, .bash_profile is our profile script
-PROFILE_SCRIPT_SHORT="~/.bash_profile"
-PROFILE_SCRIPT_FULL=~/.bash_profile
+profile_script_short="~/.bash_profile"
+profile_script_full=~/.bash_profile
 
 # Find which exists .bash_profile, .bash_login, or .profile
 if [[ -f ~/.bash_profile ]]; then
   : # Use defaults
 elif [[ -f ~/.bash_login ]]; then
-  PROFILE_SCRIPT_SHORT="~/.bash_login"
-  PROFILE_SCRIPT_FULL=~/.bash_login
+  profile_script_short="~/.bash_login"
+  profile_script_full=~/.bash_login
 elif [[ -f ~/.profile ]]; then
-  PROFILE_SCRIPT_SHORT="~/.profile"
-  PROFILE_SCRIPT_FULL=~/.profile
+  profile_script_short="~/.profile"
+  profile_script_full=~/.profile
 fi
 
 # If the current script does not have notes about .bashrc
 # DEV: Introduced due to #24, a regression that prevented users from logging in
-if ! grep .bashrc "$PROFILE_SCRIPT_FULL" &> /dev/null; then
+if ! grep .bashrc "$profile_script_full" &> /dev/null; then
   # Add a bash invocation to the profile script
-  echo "# Adding ~/.bashrc triggers to $PROFILE_SCRIPT_SHORT"
-  echo "# Trigger ~/.bashrc commands" >> "$PROFILE_SCRIPT_FULL"
-  echo ". ~/.bashrc" >> "$PROFILE_SCRIPT_FULL"
+  echo "# Adding ~/.bashrc triggers to $profile_script_short"
+  echo "# Trigger ~/.bashrc commands" >> "$profile_script_full"
+  echo ". ~/.bashrc" >> "$profile_script_full"
 
   # If our prompt is not being loaded, notify the user and leave angrily
   ps1_is_installed || (echo "sexy-bash-prompt was added to ~/.bashrc \
-and $PROFILE_SCRIPT_SHORT but is not being picked up by bash." 1>&2 && exit 1)
+and $profile_script_short but is not being picked up by bash." 1>&2 && exit 1)
 # Otherwise, notify the user about how to add it but do nothing
 else
   echo "# We cannot confirm that sexy-bash-prompt has installed properly" 1>&2
   echo "# Please open a new terminal window to confirm" 1>&2
   echo "" 1>&2
   echo "# If it has not, please run the following code:" 1>&2
-  echo "echo \". ~/.bashrc\" >> \"$PROFILE_SCRIPT_FULL\"" 1>&2
+  echo "echo \". ~/.bashrc\" >> \"$profile_script_full\"" 1>&2
   exit 1
 fi
