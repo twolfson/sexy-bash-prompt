d080ba8391c80b36048f4868ad2fa6a0e788f7c3
Todd Wolfson <todd@twolfson.com>
2014-08-25 20:33:24 +0900

    Rewrote before script to allow for any bleeding edge PPA

--- test/before_script.sh
+++ test/before_script.sh
@@ -2,7 +2,13 @@
 
 # If were are not using `git@1.8`
 if [[ "$GIT_PPA" != "ppa:git-core/v1.8" ]]; then
-  # then use the Ubuntu one
+  # Remove `git-core@1.8` PPA
   sudo apt-get install ppa-purge -y
-  sudo ppa-purge "ppa:ppa:git-core/v1.8" -y
+  sudo ppa-purge "ppa:git-core/v1.8" -y
+
+  # If we want to use a bleeding edge one
+  if [[ "$GIT_PPA" != "1.7.9.5-1" ]]; then
+    sudo add-apt-repository "$GIT_PPA"
+  fi
+  # Otherwise, use the Ubuntu one by default
 fi
