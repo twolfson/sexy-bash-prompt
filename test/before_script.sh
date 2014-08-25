213e60a8285307a8b7dfbb5c2d7d1c88593ad07a
Todd Wolfson <todd@twolfson.com>
2014-08-25 20:40:31 +0900

    Corrected option location

--- test/before_script.sh
+++ test/before_script.sh
@@ -3,12 +3,12 @@
 # If were are not using `git@1.8`
 if [[ "$GIT_PPA" != "ppa:git-core/v1.8" ]]; then
   # Remove `git-core@1.8` PPA
-  sudo apt-get install ppa-purge -y
-  sudo ppa-purge "ppa:git-core/v1.8" -y
+  sudo apt-get install -y ppa-purge
+  sudo ppa-purge -y "ppa:git-core/v1.8"
 
   # If we want to use a bleeding edge one
   if [[ "$GIT_PPA" != "1.7.9.5-1" ]]; then
-    sudo add-apt-repository "$GIT_PPA" -y
+    sudo add-apt-repository -y "$GIT_PPA"
   fi
   # Otherwise, use the Ubuntu one by default
 fi
