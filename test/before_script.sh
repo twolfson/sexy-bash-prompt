9626e82100741c02747847c6dc51bb5bcc0e843b
Todd Wolfson <todd@twolfson.com>
2014-08-25 20:29:59 +0900

    Introducing a new PPA version for the default provided by Travis CI

--- test/before_script.sh
+++ test/before_script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# If were are not on the bleeding edge git
-if [[ "$GIT_PPA" != "ppa:pdoes/ppa" ]]; then
+# If were are not using `git@1.8`
+if [[ "$GIT_PPA" != "ppa:git-core/v1.8" ]]; then
   # then use the Ubuntu one
   sudo apt-get install ppa-purge -y
-  sudo ppa-purge "ppa:pdoes/ppa" -y
+  sudo ppa-purge "ppa:ppa:git-core/v1.8" -y
 fi
