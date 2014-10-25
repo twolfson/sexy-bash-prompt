5e4f12656ea2f5119921780031e81e8ac0e516d5
Todd Wolfson <todd@twolfson.com>
2014-10-25 16:58:08 -0700

    Added generation for hexagon

---
+++ explore-unicode.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# http://stackoverflow.com/questions/602912/how-do-you-echo-a-4-digit-unicode-character-in-bash
+# Output our standard hexagon
+echo -e "\xE2\xAC\xA2"
--- explore-unicode.sh
+++
@@ -1,3 +0,0 @@
-for i in $(seq 0 $(tput colors)); do
-  echo " $(tput setaf $i)Text$(tput sgr0) $(tput bold)$(tput setaf $i)Text$(tput sgr0) $(tput sgr 0 1)$(tput setaf $i)Text$(tput sgr0)  \$(tput setaf $i)"
-done
