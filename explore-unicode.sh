2e7119f5063674589a3decf88f56a105b92cfbfe
Todd Wolfson <todd@twolfson.com>
2014-10-25 16:51:14 -0700

    Creating iterative unicode explorer

---
+++ explore-unicode.sh
@@ -0,0 +1,3 @@
+for i in $(seq 0 $(tput colors)); do
+  echo " $(tput setaf $i)Text$(tput sgr0) $(tput bold)$(tput setaf $i)Text$(tput sgr0) $(tput sgr 0 1)$(tput setaf $i)Text$(tput sgr0)  \$(tput setaf $i)"
+done
