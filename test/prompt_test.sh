af909adc4ea2e9968ac4a9fe54b940d16721b86b
Todd Wolfson <todd@twolfson.com>
2014-10-25 21:12:29 -0700

    Added status symbols tests

--- test/prompt_test.sh
+++ test/prompt_test.sh
@@ -267,3 +267,43 @@ esc=$'\033'
     # use the new colors
     expected_prompt='\[\033[1;32m\]\u\['$esc'(B'$esc'[m\] \[\033[1;33m\]at\['$esc'(B'$esc'[m\] \[\033[1;34m\]\h\['$esc'(B'$esc'[m\] \[\033[1;33m\]in\['$esc'(B'$esc'[m\] \[\033[1;35m\]\w\['$esc'(B'$esc'[m\]$( is_on_git &&   echo -n " \[\033[1;33m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\[\033[1;36m\]$(get_git_info)" &&   echo -n "\[\033[1;37m\]$(get_git_progress)" &&   echo -n "\[\033[1;33m\]")\n\['$esc'(B'$esc'[m\]\[\033[1;38m\]$ \['$esc'(B'$esc'[m\]'
     test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (overridden)' 1>&2
+
+# prompt status symbols
+  # when overridden
+  PROMPT_SYNCED_SYMBOL='#synced' PROMPT_DIRTY_SYNCED_SYMBOL='#dirty-synced' \
+    PROMPT_UNPUSHED_SYMBOL='#unpushed' PROMPT_DIRTY_UNPUSHED_SYMBOL='#dirty-unpushed' \
+    PROMPT_UNPULLED_SYMBOL='#unpulled' PROMPT_DIRTY_UNPULLED_SYMBOL='#dirty-unpulled' \
+    PROMPT_UNPUSHED_UNPULLED_SYMBOL='#unpushed-unpulled' PROMPT_DIRTY_UNPUSHED_UNPULLED_SYMBOL='#dirty-unpushed-unpulled' \
+    . .bash_prompt
+
+    # a synced prompt uses the new symbol
+    fixture_dir 'synced'
+    test "$(get_git_status)" = "#synced" || echo '`get_git_status` !== "#synced" on an overridden synced branch' 1>&2
+
+    # a dirty synced prompt uses the new symbol
+    fixture_dir 'dirty-synced'
+    test "$(get_git_status)" = "#dirty-synced" || echo '`get_git_status` !== "#dirty-synced" on an overridden dirty synced branch' 1>&2
+
+    # an unpushed prompt uses the new symbol
+    fixture_dir 'unpushed'
+    test "$(get_git_status)" = "#unpushed" || echo '`get_git_status` !== "#unpushed" on an overridden and unpushed branch' 1>&2
+
+    # a dirty and unpushed prompt uses the new symbol
+    fixture_dir 'dirty-unpushed'
+    test "$(get_git_status)" = "#dirty-unpushed" || echo '`get_git_status` !== "#dirty-unpushed" on an overridden, dirty, and unpushed branch' 1>&2
+
+    # an unpulled prompt uses the new symbol
+    fixture_dir 'unpulled'
+    test "$(get_git_status)" = "#unpulled" || echo '`get_git_status` !== "#unpulled" on an overridden and unpulled branch' 1>&2
+
+    # a dirty and unpulled prompt uses the new symbol
+    fixture_dir 'dirty-unpulled'
+    test "$(get_git_status)" = "#dirty-unpulled" || echo '`get_git_status` !== "#dirty-unpulled" on an overridden, dirty, and unpulled branch' 1>&2
+
+    # an unpushed and unpushed prompt uses the new symbol
+    fixture_dir 'unpushed-unpulled'
+    test "$(get_git_status)" = "#unpushed-unpulled" || echo '`get_git_status` !== "#unpushed-unpulled" on an overridden, unpushed, and unpulled branch' 1>&2
+
+    # a dirty, unpushed, and unpushed prompt uses the new symbol
+    fixture_dir 'dirty-unpushed-unpulled'
+    test "$(get_git_status)" = "#dirty-unpushed-unpulled" || echo '`get_git_status` !== "#dirty-unpushed-unpulled" on an overridden, dirty, unpushed, and unpulled branch' 1>&2
