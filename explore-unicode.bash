dd7c823d526cba342a288c385414402bcc59f175
Todd Wolfson <todd@twolfson.com>
2014-10-25 17:33:19 -0700

    Moving to printf

--- explore-unicode.bash
+++ explore-unicode.bash
@@ -9,28 +9,33 @@
 # echo -n "▽" | hexdump -C; # 00000000  e2 96 bd
 # echo -n "*" | hexdump -C; # 00000000  2a
 
-# # Output our symbols from their hex equivalents
-echo -e "\xE2\xAC\xA2" # Filled hexagon # In getty, diamond
-echo -e "\xE2\x96\xB2" # Filled up triangle # In getty, up arrow
-echo -e "\xE2\x96\xBC" # Filled down triangle # In getty, down arrow
-echo -e "\xE2\xAC\xA1" # Empty hexagon # In getty, diamond
-echo -e "\xE2\x96\xB3" # Empty up triangle # In getty, diamond
-echo -e "\xE2\x96\xBD" # Empty down triangle # In getty, diamond
-echo -e "\x2A" # Asterisk # In getty, asterisk
+# # # Output our symbols from their hex equivalents
+# echo -e "\xE2\xAC\xA2" # Filled hexagon # In getty, diamond
+# echo -e "\xE2\x96\xB2" # Filled up triangle # In getty, up arrow
+# echo -e "\xE2\x96\xBC" # Filled down triangle # In getty, down arrow
+# echo -e "\xE2\xAC\xA1" # Empty hexagon # In getty, diamond
+# echo -e "\xE2\x96\xB3" # Empty up triangle # In getty, diamond
+# echo -e "\xE2\x96\xBD" # Empty down triangle # In getty, diamond
+# echo -e "\x2A" # Asterisk # In getty, asterisk
 
-# $ echo -n "↑" | hexdump -C
-# 00000000  e2 86 91                                          |...|
-# 00000003
-# todd at Euclid in ~/github/sexy-bash-prompt on dev/explore.unicode*
-# $ echo -n "˄" | hexdump -C
-# 00000000  cb 84                                             |..|
-# 00000002
+# # $ echo -n "↑" | hexdump -C
+# # 00000000  e2 86 91                                          |...|
+# # 00000003
+# # todd at Euclid in ~/github/sexy-bash-prompt on dev/explore.unicode*
+# # $ echo -n "˄" | hexdump -C
+# # 00000000  cb 84                                             |..|
+# # 00000002
 
-echo -e "\xE2\x86\x91" # Works but not sure why such a high range one does
-echo -e "\xE2\x86" # Doesn't work
-echo -e "\xE2\x91" # Doens't work
-echo -e "\x86\x91" # Doens't work
-echo -e "\xE2" # Doens't work
-echo -e "\x86" # Doens't work
-echo -e "\x91" # Doens't work
-# echo -e "\xCB\x84" # Doesn't work
+# echo -e "\xE2\x86\x91" # Works but not sure why such a high range one does
+# echo -e "\xE2\x86" # Doesn't work
+# echo -e "\xE2\x91" # Doens't work
+# echo -e "\x86\x91" # Doens't work
+# echo -e "\xE2" # Doens't work
+# echo -e "\x86" # Doens't work
+# echo -e "\x91" # Doens't work
+# # echo -e "\xCB\x84" # Doesn't work
+
+# http://unix.stackexchange.com/questions/15139/how-to-print-all-printable-ascii-chars-in-cli
+for ((i=32;i<=127;i++)); do
+  printf "\\$((i/64*100+i%64/8*10+i%8))\t";
+done
