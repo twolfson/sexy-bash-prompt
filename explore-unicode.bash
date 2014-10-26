387fe75ddb4dba478f9256d37bb8a53cf7ce82f2
Todd Wolfson <todd@twolfson.com>
2014-10-25 17:01:29 -0700

    Adding sample for interpretting hex

--- explore-unicode.bash
+++ explore-unicode.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # http://stackoverflow.com/questions/602912/how-do-you-echo-a-4-digit-unicode-character-in-bash
-# Output our standard hexagon
+# Output our filled hexagon
+# echo -n "⬢" | hexdump -C; # 00000000  e2 ac a2
 echo -e "\xE2\xAC\xA2"
