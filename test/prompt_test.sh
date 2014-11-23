137a5c069d4c4fdccfbc36fba020ee759f59149f
Todd Wolfson <todd@twolfson.com>
2014-11-22 18:39:58 -0800

    Fixed up test script

--- test/prompt_test.sh
+++ test/prompt_test.sh
@@ -73,14 +73,14 @@ fixture_git_init() {
 
 # git_status
 
-  # on a clean and synced branch
-  fixture_dir 'clean-synced'
+  # on a synced branch
+  fixture_dir 'synced'
 
     # is nothing
     test "$(prompt_get_git_status)" = "" || echo '`prompt_get_git_status` !== "" on a synced branch' 1>&2
 
-  # on a dirty branch
-  fixture_dir 'dirty'
+  # on a dirty synced branch
+  fixture_dir 'dirty-synced'
 
     # is an asterisk
     test "$(prompt_get_git_status)" = "*" || echo '`prompt_get_git_status` !== "*" on a dirty synced branch' 1>&2
@@ -238,7 +238,7 @@ esc=$'\033'
     # test "$prompt_user_color" = "$esc[1m$esc[38;5;27m" || echo '`prompt_user_color` is not bold blue (256)' 1>&2
 
     # uses 256 color palette
-    expected_prompt='\['$esc'[1m'$esc'[38;5;27m\]\u\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]at\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;39m\]\h\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]in\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;76m\]\w\['$esc'(B'$esc'[m\]$( prompt_is_on_git &&   echo -n " \['$esc'[1m'$esc'[37m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\['$esc'[1m'$esc'[38;5;154m\]$(get_git_info)" &&   echo -n "\['$esc'[1m'$esc'[91m\]$(prompt_get_git_progress)" &&   echo -n "\['$esc'[1m'$esc'[37m\]")\n\['$esc'(B'$esc'[m\]\['$esc'[1m\]$ \['$esc'(B'$esc'[m\]'
+    expected_prompt='\['$esc'[1m'$esc'[38;5;27m\]\u\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]at\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;39m\]\h\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]in\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;76m\]\w\['$esc'(B'$esc'[m\]$( prompt_is_on_git &&   echo -n " \['$esc'[1m'$esc'[37m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\['$esc'[1m'$esc'[38;5;154m\]$(prompt_get_git_info)" &&   echo -n "\['$esc'[1m'$esc'[91m\]$(prompt_get_git_progress)" &&   echo -n "\['$esc'[1m'$esc'[37m\]")\n\['$esc'(B'$esc'[m\]\['$esc'[1m\]$ \['$esc'(B'$esc'[m\]'
 
     # DEV: To debug, use a diff tool. Don't stare at the code.
     # http://www.diffchecker.com/diff
@@ -251,19 +251,59 @@ esc=$'\033'
   TERM=xterm . .bash_prompt
 
     # uses 8 color palette
-    expected_prompt='\['$esc'[1m'$esc'[34m\]\u\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]at\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[36m\]\h\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]in\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[32m\]\w\['$esc'(B'$esc'[m\]$( prompt_is_on_git &&   echo -n " \['$esc'[1m'$esc'[37m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\['$esc'[1m'$esc'[33m\]$(get_git_info)" &&   echo -n "\['$esc'[1m'$esc'[31m\]$(prompt_get_git_progress)" &&   echo -n "\['$esc'[1m'$esc'[37m\]")\n\['$esc'(B'$esc'[m\]\['$esc'[1m\]$ \['$esc'(B'$esc'[m\]'
+    expected_prompt='\['$esc'[1m'$esc'[34m\]\u\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]at\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[36m\]\h\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]in\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[32m\]\w\['$esc'(B'$esc'[m\]$( prompt_is_on_git &&   echo -n " \['$esc'[1m'$esc'[37m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\['$esc'[1m'$esc'[33m\]$(prompt_get_git_info)" &&   echo -n "\['$esc'[1m'$esc'[31m\]$(prompt_get_git_progress)" &&   echo -n "\['$esc'[1m'$esc'[37m\]")\n\['$esc'(B'$esc'[m\]\['$esc'[1m\]$ \['$esc'(B'$esc'[m\]'
     test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (8)' 1>&2
 
   # in an ANSI terminal
   TERM="" . .bash_prompt
 
     # uses ANSI colors
-    expected_prompt='\[\033[1;34m\]\u\[\033[m\] \[\033[1;37m\]at\[\033[m\] \[\033[1;36m\]\h\[\033[m\] \[\033[1;37m\]in\[\033[m\] \[\033[1;32m\]\w\[\033[m\]$( prompt_is_on_git &&   echo -n " \[\033[1;37m\]on\[\033[m\] " &&   echo -n "\[\033[1;33m\]$(get_git_info)" &&   echo -n "\[\033[1;31m\]$(prompt_get_git_progress)" &&   echo -n "\[\033[1;37m\]")\n\[\033[m\]\[\]$ \[\033[m\]'
+    expected_prompt='\[\033[1;34m\]\u\[\033[m\] \[\033[1;37m\]at\[\033[m\] \[\033[1;36m\]\h\[\033[m\] \[\033[1;37m\]in\[\033[m\] \[\033[1;32m\]\w\[\033[m\]$( prompt_is_on_git &&   echo -n " \[\033[1;37m\]on\[\033[m\] " &&   echo -n "\[\033[1;33m\]$(prompt_get_git_info)" &&   echo -n "\[\033[1;31m\]$(prompt_get_git_progress)" &&   echo -n "\[\033[1;37m\]")\n\[\033[m\]\[\]$ \[\033[m\]'
     test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (ANSI)' 1>&2
 
   # when overridden
   TERM=xterm-256color PROMPT_USER_COLOR='\033[1;32m' PROMPT_PREPOSITION_COLOR='\033[1;33m' PROMPT_DEVICE_COLOR='\033[1;34m' PROMPT_DIR_COLOR='\033[1;35m' PROMPT_GIT_STATUS_COLOR='\033[1;36m'  PROMPT_GIT_PROGRESS_COLOR='\033[1;37m' PROMPT_SYMBOL_COLOR='\033[1;38m' . .bash_prompt
 
     # use the new colors
-    expected_prompt='\[\033[1;32m\]\u\['$esc'(B'$esc'[m\] \[\033[1;33m\]at\['$esc'(B'$esc'[m\] \[\033[1;34m\]\h\['$esc'(B'$esc'[m\] \[\033[1;33m\]in\['$esc'(B'$esc'[m\] \[\033[1;35m\]\w\['$esc'(B'$esc'[m\]$( prompt_is_on_git &&   echo -n " \[\033[1;33m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\[\033[1;36m\]$(get_git_info)" &&   echo -n "\[\033[1;37m\]$(prompt_get_git_progress)" &&   echo -n "\[\033[1;33m\]")\n\['$esc'(B'$esc'[m\]\[\033[1;38m\]$ \['$esc'(B'$esc'[m\]'
+    expected_prompt='\[\033[1;32m\]\u\['$esc'(B'$esc'[m\] \[\033[1;33m\]at\['$esc'(B'$esc'[m\] \[\033[1;34m\]\h\['$esc'(B'$esc'[m\] \[\033[1;33m\]in\['$esc'(B'$esc'[m\] \[\033[1;35m\]\w\['$esc'(B'$esc'[m\]$( prompt_is_on_git &&   echo -n " \[\033[1;33m\]on\['$esc'(B'$esc'[m\] " &&   echo -n "\[\033[1;36m\]$(prompt_get_git_info)" &&   echo -n "\[\033[1;37m\]$(prompt_get_git_progress)" &&   echo -n "\[\033[1;33m\]")\n\['$esc'(B'$esc'[m\]\[\033[1;38m\]$ \['$esc'(B'$esc'[m\]'
     test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (overridden)' 1>&2
+
+# prompt status symbols
+  # when overridden
+  PROMPT_SYNCED_SYMBOL='#synced' PROMPT_DIRTY_SYNCED_SYMBOL='#dirty-synced' \
+    PROMPT_UNPUSHED_SYMBOL='#unpushed' PROMPT_DIRTY_UNPUSHED_SYMBOL='#dirty-unpushed' \
+    PROMPT_UNPULLED_SYMBOL='#unpulled' PROMPT_DIRTY_UNPULLED_SYMBOL='#dirty-unpulled' \
+    PROMPT_UNPUSHED_UNPULLED_SYMBOL='#unpushed-unpulled' PROMPT_DIRTY_UNPUSHED_UNPULLED_SYMBOL='#dirty-unpushed-unpulled' \
+    . .bash_prompt
+
+    # a synced prompt uses the new symbol
+    fixture_dir 'synced'
+    test "$(prompt_get_git_status)" = "#synced" || echo '`prompt_get_git_status` !== "#synced" on an overridden synced branch' 1>&2
+
+    # a dirty synced prompt uses the new symbol
+    fixture_dir 'dirty-synced'
+    test "$(prompt_get_git_status)" = "#dirty-synced" || echo '`prompt_get_git_status` !== "#dirty-synced" on an overridden dirty synced branch' 1>&2
+
+    # an unpushed prompt uses the new symbol
+    fixture_dir 'unpushed'
+    test "$(prompt_get_git_status)" = "#unpushed" || echo '`prompt_get_git_status` !== "#unpushed" on an overridden and unpushed branch' 1>&2
+
+    # a dirty and unpushed prompt uses the new symbol
+    fixture_dir 'dirty-unpushed'
+    test "$(prompt_get_git_status)" = "#dirty-unpushed" || echo '`prompt_get_git_status` !== "#dirty-unpushed" on an overridden, dirty, and unpushed branch' 1>&2
+
+    # an unpulled prompt uses the new symbol
+    fixture_dir 'unpulled'
+    test "$(prompt_get_git_status)" = "#unpulled" || echo '`prompt_get_git_status` !== "#unpulled" on an overridden and unpulled branch' 1>&2
+
+    # a dirty and unpulled prompt uses the new symbol
+    fixture_dir 'dirty-unpulled'
+    test "$(prompt_get_git_status)" = "#dirty-unpulled" || echo '`prompt_get_git_status` !== "#dirty-unpulled" on an overridden, dirty, and unpulled branch' 1>&2
+
+    # an unpushed and unpushed prompt uses the new symbol
+    fixture_dir 'unpushed-unpulled'
+    test "$(prompt_get_git_status)" = "#unpushed-unpulled" || echo '`prompt_get_git_status` !== "#unpushed-unpulled" on an overridden, unpushed, and unpulled branch' 1>&2
+
+    # a dirty, unpushed, and unpushed prompt uses the new symbol
+    fixture_dir 'dirty-unpushed-unpulled'
+    test "$(prompt_get_git_status)" = "#dirty-unpushed-unpulled" || echo '`prompt_get_git_status` !== "#dirty-unpushed-unpulled" on an overridden, dirty, unpushed, and unpulled branch' 1>&2
