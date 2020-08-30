80ffd2ca40a6ae1713839feafa203d1e863d1a1d
Todd Wolfson <todd@twolfson.com>
2020-08-29 21:27:39 -0700

    Added exit code support (#89)
    * Added exit code/error color logic
    
    * Happy accident around dumping exit code
    
    * Removed exit code from prompt for now
    
    * Moved to consistent number comparison
    
    * Added logic around repeating the same command/exit code
    
    * Added clarification around last command
    
    * Documented new ERROR_COLOR setting
    
    * Added PROMPT_COMMAND call to tests
    
    * Added Travis CI notification on status change
    
    * Adding in error prompt color test
    
    * Finished test
    
    * Started adding in missing command config but feel like I'm overbuilding/overwording
    
    * Added documentation but feels verbose as mentioned
    
    * Updated functionality explainer
    
    * Moved to more obvious configuration definition
    
    * Moved to more straightforward SHOW_ERROR_ONCE option

--- test/prompt_test.sh
+++ test/prompt_test.sh
@@ -308,6 +308,17 @@ esc=$'\033'
     expected_prompt='\['$esc'(B'$esc'[m\]\[\033[1;32m\]\u\['$esc'(B'$esc'[m\] \[\033[1;33m\]at\['$esc'(B'$esc'[m\] \[\033[1;34m\]\h\['$esc'(B'$esc'[m\] \[\033[1;33m\]in\['$esc'(B'$esc'[m\] \[\033[1;35m\]\w\['$esc'(B'$esc'[m\] \[\033[1;33m\]on\['$esc'(B'$esc'[m\] \[\033[1;36m\]$(sexy_bash_prompt_get_git_info)\[\033[1;37m\]$(sexy_bash_prompt_get_git_progress)\['$esc'(B'$esc'[m\]\n\[\033[1;38m\]$ \['$esc'(B'$esc'[m\]'
     test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (overridden)' 1>&2
 
+  # with an error code
+  TERM=xterm-256color . .bash_prompt
+  # DEV: Stub out last command being checked against
+  sexy_bash_prompt_last_command="invalid command"
+  false
+  $PROMPT_COMMAND
+
+    # uses red prompt symbol
+    expected_prompt='\['$esc'(B'$esc'[m\]\['$esc'[1m'$esc'[38;5;27m\]\u\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]at\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;39m\]\h\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]in\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;76m\]\w\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[37m\]on\['$esc'(B'$esc'[m\] \['$esc'[1m'$esc'[38;5;154m\]$(sexy_bash_prompt_get_git_info)\['$esc'[1m'$esc'[91m\]$(sexy_bash_prompt_get_git_progress)\['$esc'(B'$esc'[m\]\n\['$esc'[1m'$esc'[31m\]$ \['$esc'(B'$esc'[m\]'
+    test "$PS1" = "$expected_prompt" || echo '`PS1` is not as expected (error-code)' 1>&2
+
 # prompt status symbols
   # when overridden
   PROMPT_SYNCED_SYMBOL='#synced' PROMPT_DIRTY_SYNCED_SYMBOL='#dirty-synced' \
